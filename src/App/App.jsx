754012111e9ba77a415bd09ed752c2b897de5f80
AlexeyRyabikov <alex-r2000@mail.ru>
2023-08-02 17:14:30 +0300

    modified:   .eslintrc.json 	modified:   .husky/pre-commit 	new file:   .idea/codeStyles/codeStyleConfig.xml 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	deleted:    src/App/App.css 	modified:   src/App/App.jsx 	new file:   src/App/App.scss 	modified:   src/index.css 	modified:   src/index.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,72 @@
-import "./App.css";
+import "./App.scss";
 import React from "react";
-
+import { styled } from "@mui/material/styles";
+// import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Tabs } from "@mui/material";
+import Tab from "@mui/material/Tab";
+const StyledTabs = styled((props) => (
+  <Tabs
+    {...props}
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+  />
+))({
+  "& .MuiTabs-indicator": {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+  },
+  "& .MuiTabs-indicatorSpan": {
+    maxWidth: 40,
+    width: "100%",
+    backgroundColor: "#635ee7",
+  },
+});
+const StyledTab = styled((props) => {
+  console.log(props);
+  return <Tab {...props} />;
+})(({ theme }) => ({
+  textTransform: "none",
+  fontWeight: theme.typography.fontWeightRegular,
+  fontSize: theme.typography.pxToRem(15),
+  marginRight: theme.spacing(1),
+  color: "rgba(255, 255, 255, 0.7)",
+  "&.Mui-selected": {
+    color: "#fff",
+  },
+  "&.Mui-focusVisible": {
+    backgroundColor: "rgba(100, 95, 228, 0.32)",
+  },
+}));
 function App() {
-  return <div className="App">FFFFFFFF</div>;
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs="auto">
+          <div className="item">
+            <div className="item2" />
+          </div>
+        </Grid>
+        <Grid item xs>
+          <div>
+            <StyledTabs
+              value={value}
+              onChange={handleChange}
+              aria-label="styled tabs example"
+            >
+              <StyledTab label="fff" />
+              <StyledTab label="Datasets" />
+              <StyledTab label="Connections" />
+            </StyledTabs>
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
-
 export default App;
